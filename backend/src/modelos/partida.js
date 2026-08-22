8b9a0fdaff5ef517b62310b79a78863af3906f1c
Umana004 <159400472+Umana004@users.noreply.github.com>
2026-08-22 01:11:03 -0600

    Se implementa funcionalidad del servicio de partidas

--- backend/src/modelos/partida.js
+++ backend/src/modelos/partida.js
@@ -7,6 +7,7 @@ class Partida {
         this.jugador_1 = jugador_1; 
         this.jugador_2 = jugador_2;
         this.rondas = [];
+        this.ronda_actual = 0;
         this.estado = "en_curso";
 
     }
--- backend/src/servicios/servicio_partida.js
+++ backend/src/servicios/servicio_partida.js
@@ -1,22 +1,21 @@
-const Jugador = require("../modelos/jugador")
-const Partida = require("../modelos/partida")
+const Jugador = require("../modelos/jugador");
+const Partida = require("../modelos/partida");
 const Ronda = require("../modelos/ronda");
-const servicio_palabras = require("./servicio_palabras")
+const servicio_palabras = require("./servicio_palabras");
+const servicio_intentos = require("./servicio_intentos");
 
-function crear_partida(nombre_jugador_1, nombre_jugador_2){
-
+function crear_partida(nombre_jugador_1, nombre_jugador_2) {
     const jugador_1 = new Jugador(nombre_jugador_1);
     const jugador_2 = new Jugador(nombre_jugador_2);
 
-    if (matchMedia.random() < 0.5){
+    if (Math.random() < 0.5) {
         return new Partida(jugador_1, jugador_2);
     }
 
     return new Partida(jugador_2, jugador_1);
 }
 
-function crear_ronda(jugador_adivinador){
-
+function crear_ronda(jugador_adivinador) {
     const palabra_secreta = servicio_palabras.obtener_palabra_aleatoria();
 
     const ronda = new Ronda(
@@ -27,7 +26,24 @@ function crear_ronda(jugador_adivinador){
     return ronda;
 }
 
-moodule.exports = {
+function iniciar_ronda(partida, jugador_adivinador) {
+    const ronda = crear_ronda(jugador_adivinador);
+
+    partida.rondas.push(ronda);
+
+    return ronda;
+}
+
+function procesar_intento(ronda, intento) {
+    return servicio_intentos.procesar_intento(
+        ronda,
+        intento
+    );
+}
+
+module.exports = {
     crear_partida,
-    crear_ronda
+    crear_ronda,
+    iniciar_ronda,
+    procesar_intento
 };
