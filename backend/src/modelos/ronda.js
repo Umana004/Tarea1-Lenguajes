1602597b66075bec2afcbe825fe03166a947effe
Umana004 <159400472+Umana004@users.noreply.github.com>
2026-08-21 23:17:17 -0600

    Se agrega modelo para las rondas

---
+++ backend/src/modelos/ronda.js
@@ -0,0 +1,15 @@
+class Ronda {
+
+    constructor(jugador_adivinador, palabra_secreta){
+        
+        this.jugador_adivinador = jugador_adivinador;
+        this.palabra_secreta = palabra_secreta;
+        this.intentos = 0;
+        this.tiempo = 0;
+        this.completada = false;
+
+    }
+
+}
+
+moodule.exports = Ronda;
