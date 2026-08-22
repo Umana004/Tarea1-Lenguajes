27fbb26321bda09ae2478f501014700e094e7350
Umana004 <159400472+Umana004@users.noreply.github.com>
2026-08-22 00:07:38 -0600

    Se crea la seleccion aleatoria del jugador que inicia

--- backend/src/servicios/servicio_partida.js
+++ backend/src/servicios/servicio_partida.js
@@ -6,9 +6,11 @@ function crear_partida(nombre_jugador_1, nombre_jugador_2){
     const jugador_1 = new Jugador(nombre_jugador_1);
     const jugador_2 = new Jugador(nombre_jugador_2);
 
-    const partida = new Partida(jugador_1, jugador_2);
+    if (matchMedia.random() < 0.5){
+        return new Partida(jugador_1, jugador_2);
+    }
 
-    return partida;
+    return new Partida(jugador_2, jugador_1);
 }
 
 moodule.exports = {
