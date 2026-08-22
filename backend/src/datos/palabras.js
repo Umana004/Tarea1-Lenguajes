307577736dd797ec71721ac976f9bf99ad40bf11
Umana004 <159400472+Umana004@users.noreply.github.com>
2026-08-22 00:16:47 -0600

    Se crea el banco de palabras a utilizar

---
+++ backend/src/datos/palabras.js
@@ -0,0 +1,16 @@
+const palabras = [
+    "casa",
+    "perro",
+    "gato",
+    "mesa",
+    "libro",
+    "playa",
+    "carro",
+    "arbol",
+    "computadora",
+    "programa",
+    "servidor",
+    "ventana"
+];
+
+module.exports = palabras;
